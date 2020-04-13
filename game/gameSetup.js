a093e96bb6df33f2304baba7348b4b8559437a91
alscotty <alawrencescott@gmail.com>
2020-04-12 20:01:05 -0700

    moving

--- game/gameSetup.js
+++ game/gameSetup.js
@@ -2,26 +2,57 @@ var config = {
     type: Phaser.Auto,
     width: 720,
     height: 600,
+    physics:{
+        default: 'arcade'
+    },
     scene:{
         preload:preload,
         create:create,
-        updated:updated
+        update:update
     }
 }
+
+var player;
+var cursors;
 
 var game = new Phaser.Game(config);
 
 function preload(){
     this.load.image('dungeon', '../assets/dungeon.png');
-    this.load.spritesheet('char', '../assets/sprites.png', {frameWidth:32, frameHeight:48});
+    this.load.spritesheet('player', '../assets/sprites.png', {frameWidth:64, frameHeight:66});
 }
+
 
 function create(){
-    this.add.image(360,300,'dungeon').setScale(1.1)
+    this.add.image(360,300,'dungeon').setScale(1.1);
 
+    player = this.physics.add.sprite(100,450,'player');
+    player.setCollideWorldBounds(true);
 
+    this.anims.create({
+        key:'spread',
+        frames: this.anims.generateFrameNumbers('player', {start:11, end: 19}),
+        frameRate: 10,
+        // repeat: -1
+    });
+    this.anims.create({
+        key:'up',
+        frames: this.anims.generateFrameNumbers('player', { start: 52, end: 59 }),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
-function updated(){
-
+function update(){
+    if (cursors.space.isDown) {
+        player.anims.play('spread',true)
+    } else if(cursors.up.isDown) {
+        player.anims.play('up', true);
+        player.setVelocityY(-100)
+    } else {
+        player.setVelocityX(0);
+        player.setVelocityY(0);
+    }
 }
